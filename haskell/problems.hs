c7fabd2818b9c641c975ae17d0b7e23690dcf2c9
Vihang Dudhalkar <vihang.dudhalkar@gmail.com>
2010-03-16 19:56:19 +0530

    Added solution for problem 4 in haskell

--- haskell/problems.hs
+++ haskell/problems.hs
@@ -8,3 +8,5 @@ problem_2 = sum [x|x <- f [1,1], even x]
         | x >= 4000000 = x:x1:xs
         | otherwise = f (x+x1:x:x1:xs)
 
+-- Problem 4
+problem_4 = maximum [z|x<-[100..999],y<-[x..999],let z =x*y, show z == reverse( show z)]
